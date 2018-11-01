af6c36b20e8952ced5159c7d7082c6bae6bcbc51
Ramiro2709 <vargasramiro99@gmail.com>
2018-10-31 22:31:52 -0300

    comentario

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {DetailPage} from '../detail/detail';
 import { HttpClient } from '@angular/common/http'; //// Para que tome al php
+import { HttpHeaders } from '@angular/common/http';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -10,23 +12,46 @@ import { HttpClient } from '@angular/common/http'; //// Para que tome al php
 export class HomePage {
   items: any[];
   ip_wamp: string ;
+  //localidad : object ;
+  header : object ;
+  //localidad_get : string ;
   constructor(public navCtrl: NavController, public http: HttpClient) {
+    //this.localidad = 'Rio Gallegos';
+    /*
+    this.localidad = {
+      'localidad' : 'TELLIER'
+    };
+    */
+  
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': ''
+      })
+    };
+
+    //this.localidad_get = 'Rio%20Gallegos';
     this.items = [];
+    //this.ip_wamp = 'http://localhost/pruebas/Ionic/prueba.php?localidad='+this.localidad_get; //IP local
     this.ip_wamp = 'http://localhost/pruebas/Ionic/prueba.php'; //IP local
+    //this.http.get(this.ip_wamp)
     this.http.get(this.ip_wamp)
     .subscribe((data : any) =>
       {
+        console.log(this.ip_wamp);
         console.log("Encontro IP");
-        this.load();
+        //this.load(localidad);
       },
       (error : any) =>
       {
-        console.log("No encontro, corregido");
+        console.log("No encontro, corregir");
+        console.log(this.ip_wamp);
         this.ip_wamp = prompt("IP incorrecta, ingrese otra", "http://192.168.0.34/pruebas/Ionic/prueba.php");
         // "http://xxx.xxx.x.xxx/pruebas/Ionic/prueba.php"
         //this.ip = 'http://localhost/pruebas/Ionic/prueba.php';
         //alert(this.ip);
-        this.load();
+        //this.load(localidad);
       });
   }
 
@@ -37,14 +62,15 @@ export class HomePage {
     })
   }
 
-  load(): void{
+  load(input_loc): void{
+    var localidad = JSON.stringify({localidad: input_loc});
     for(let i = 0; i<10;i++){
       //alert(this.ip);
       this.http
-      .get(this.ip_wamp)
+      .post<string>(this.ip_wamp,localidad)
       .subscribe((data : any) =>
       {
-         console.dir(data);
+         //console.dir(data);
          this.items.push({
           text: data[i],
           id: i
